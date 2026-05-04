bc1cae695669f406c97026697e4e7f16a647dba2
miraclemint <u.tharin@gmail.com>
2026-05-04 22:44:40 +0700

    Add theme toggle functionality and icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
-console.log("Mint Tharin Portfolio Loaded");
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('light-mode');
+});
